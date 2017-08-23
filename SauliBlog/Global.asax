b636bdafc69c393ea7414627b2daac2417ad6e18
Tomer Sulimany <tomer.suli1@gmail.com>
2017-08-24 01:18:27 +0300

    First commit

---
+++ SauliBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SauliBlog.MvcApplication" Language="C#" %>
---
+++ SauliBlog/Models/Fan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SauliBlog.Models
+{
+    public class Fan
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Seniority { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male, Female
+    }
+}
