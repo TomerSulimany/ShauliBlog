2cf9814dfe501d16ed4895c0dcff4eb414a660cd
Tomer Sulimany <tomer.suli1@gmail.com>
2017-08-30 00:08:56 +0300

    Added Fan

--- SauliBlog/Controllers/FanController.cs
+++ SauliBlog/Controllers/FanController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SauliBlog.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,96 @@ namespace SauliBlog.Controllers
 {
     public class FanController : Controller
     {
+        static List<Fan> Fans = new List<Fan>();
         // GET: Fan
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: First/Details
+        public ActionResult Details()
+        {
+            if (Fans.Count == 0)
+            {
+                Fan f = new Fan { Birthday = new DateTime(1995, 12 ,12), Gender = Gender.Male, Name = "Tomer", Seniority = 1, Surname = "Sulimany" };
+                Fans.Add(f);
+            }
+            return View(Fans);
+        }
+
+        // GET: First/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: First/Create
+        [HttpPost]
+        public ActionResult Create(Fan fan)
+        {
+            try
+            {
+                Fans.Add(fan);
+
+                return RedirectToAction("Details");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: First/Edit/5
+        public ActionResult Edit(int id)
+        {
+            foreach (Fan emp in Fans)
+            {
+                if (emp.Name.Equals(id))
+                {
+                    return View(emp);
+                }
+            }
+            return View("Error");
+        }
+
+        // POST: First/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Fan empT)
+        {
+            try
+            {
+                foreach (Fan emp in Fans)
+                {
+                    if (emp.ID.Equals(id))
+                    {
+                        emp.copy(empT);
+                        return RedirectToAction("Index");
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        // GET: First/Delete/5
+        public ActionResult Delete(int id)
+        {
+            int i = 0;
+            foreach (Fan emp in Fans)
+            {
+                if (emp.ID.Equals(id))
+                {
+                    Fans.RemoveAt(i);
+                    return RedirectToAction("Details");
+                }
+                i++;
+            }
+            return RedirectToAction("Error");
         }
     }
 }
--- SauliBlog/Models/Fan.cs
+++ SauliBlog/Models/Fan.cs
@@ -1,17 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace SauliBlog.Models
 {
-    public class Fan
+    public class Fan : IEquatable<Fan>
     {
+        static int count = 0;
+
+        [Required]
+        [Display(Name = "ID")]
+        public int ID { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Name")]
         public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Surname")]
         public string Surname { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
         public Gender Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Birthday")]
         public DateTime Birthday { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Seniority")]
         public int Seniority { get; set; }
+
+        public Fan()
+        {
+            count++;
+            ID = count;
+        }
+
+        public void copy(Fan fan)
+        {
+            ID = fan.ID;
+            Name = fan.Name;
+            Surname = fan.Surname;
+            Gender = fan.Gender;
+            Birthday = fan.Birthday;
+            Seniority = fan.Seniority;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj as Fan);
+        }
+
+        public bool Equals(Fan other)
+        {
+            if (other == null)
+                return false;
+
+            return (this.ID == other.ID);
+        }
     }
 
     public enum Gender
