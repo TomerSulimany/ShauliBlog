12827f0c6e6f4e8fa5336c2de34be27e58ee0bfc
DESKTOP-QTAIIBU\User <elad121@gmail.com>
2017-10-06 01:15:13 +0300

    Adding autocompleet to the input text of the Author in the create file and in the index file (AJAX)

---
+++ SauliBlog/Scripts/FanAutoComplete.js
@@ -0,0 +1,53 @@
+﻿
+$(document).ready(function () {
+
+    var in_suggestion_foucos = false;
+
+    $("#Author").focusout(function () {
+        if (!in_suggestion_foucos)
+        { $("#author-suggesstion-box").hide();}
+    })
+
+    $("#Author").keyup(function (){
+
+    var serviceURL = '/Fan/AutoCompleteFan';
+
+    $.ajax({
+        type: "POST",
+        url: serviceURL,
+        data: { txt: document.getElementById("Author").value },
+        success: successFunc,
+        error: errorFunc
+    });
+
+    function successFunc(data, status) {
+
+    $("#author-suggesstion-box").show();
+    $("#author-suggesstion-box").html(data);
+    $("#Author").css("background", "#FFF");
+
+    $(".fan-suggestion-option").click(function () {
+        $("#Author").val($(this).attr("name"));
+        $("#author-suggesstion-box").hide();
+    });
+
+    $("#fan-suggestion-list").mouseover(function () {
+        $("#author-suggesstion-box").show();
+        in_suggestion_foucos = true;
+    });
+
+    $("#fan-suggestion-list").mouseout(function () {
+        $("#author-suggesstion-box").hide();
+        in_suggestion_foucos = false;
+    });
+
+    
+
+    }
+
+    function errorFunc() {
+    }
+    })
+
+
+})
