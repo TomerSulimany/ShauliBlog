e63fb8b2b1b54de26bf9da843446c98efb1b7139
DESKTOP-QTAIIBU\User <elad121@gmail.com>
2017-10-08 22:51:20 +0300

    Creating new db

--- SauliBlog/Models/BlogDbContext.cs
+++ SauliBlog/Models/BlogDbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 
@@ -16,5 +17,15 @@ namespace ShauliBlog.Models
 
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Fan> Fans { get; set; }
     }
+    /*
+    public class MyDbConfiguration : DbMigrationsConfiguration<BlogDbContext>
+    {
+        public MyDbConfiguration()
+        {
+            this.AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = true;
+        }
+    }*/
 }
--- SauliBlog/Models/Fan.cs
+++ SauliBlog/Models/Fan.cs
@@ -73,4 +73,5 @@ namespace ShauliBlog.Models
     {
         Male, Female
     }
+
 }
