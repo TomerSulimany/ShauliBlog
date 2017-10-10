084e49dd525b75d8deaa1f00e9ad3146bb6809bd
Tomer Sulimany <tomer.suli1@gmail.com>
2017-10-10 12:16:04 +0300

    Added food rss, applied a migration that was not updated

--- SauliBlog/Migrations/Configuration.cs
+++ SauliBlog/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace SauliBlog.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(ShauliBlog.Models.BlogDbContext context)
