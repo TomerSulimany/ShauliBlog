af9a1cac769b8680d2840c07f67c072dbeb1ceab
LENOVO-PC\USER <eladm1993@gmail.com>
2017-10-23 10:26:05 +0300

    add the statistic for post also the recomend post that isn't work yet

--- SauliBlog/Controllers/BlogController.cs
+++ SauliBlog/Controllers/BlogController.cs
@@ -20,6 +20,7 @@ namespace ShauliBlog.Controllers
             return View(db.Posts.ToList());
         }
 
+
         // POST: Blog/CommentAdd
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -44,5 +45,74 @@ namespace ShauliBlog.Controllers
 
             return RedirectToAction("Index");
         }
+
+
+        public class PostWithFans
+        {
+            public Post post { get; set; }
+            public string FanName { get; set; }
+        }
+
+        public class CountPostObj
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Count { get; set; }
+        }
+
+        public IEnumerable<PostWithFans> JoinPostWithFans()
+        {
+            var q = db.Posts.Join(db.Fans,
+                p => p.FanUser,
+                f => f.ID,
+                (p, f) => new PostWithFans()
+                {
+                    post = p,
+                    FanName = f.Name + " " + f.Surname
+                });
+
+            return q;
+        }
+
+        [HttpPost, ActionName("CountPostByWriter")]
+        public ActionResult CountPostByWriter()
+        {
+            var q = db.Posts.GroupBy(p => new { Name = p.Author })
+                .Select(p => new CountPostObj()
+                {
+                    Name = p.Key.Name,
+                    Count = p.Count()
+                });
+            ViewBag.CountPostByWriter = q.ToList();
+            ViewBag.Posts = JoinPostWithFans().ToList();
+
+            return View("SimpleStatistics");
+        }
+
+        [HttpPost, ActionName("RecommendPost")]
+        public ActionResult RecommendPost(string first_name, string last_name)
+        {
+            ViewBag.Context = "Recommened Posts For You";
+            ViewBag.Comments = db.Comments.ToList();
+            ViewBag.Posts = new List<PostWithFans>();
+
+            var writerPosts = JoinPostWithFans().Where(p => p.post.Author == first_name + " " + last_name);
+            var nonWriterPosts = JoinPostWithFans().Where(p => p.post.Author != first_name + " " + last_name);
+            foreach (var obj1 in nonWriterPosts)
+            {
+                var post = obj1.post;
+                foreach (var obj2 in writerPosts)
+                {
+                    var writerPost = obj2.post;
+                    if (post.Content.ToLower().Contains(writerPost.Title.ToLower()))
+                    {
+                        ViewBag.Posts.Add(obj1);
+                    }
+                }
+            }
+
+            return View("Index");
+        }
+
     }
 }
